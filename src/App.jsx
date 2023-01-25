1c0e624a3b8a420fe4dfe5fb3df80dfdfefb7b29
umair-khalid-brainx <umair.khalid@brainxtech.com>
2023-01-25 13:06:21 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './stylesheets/App.css';
+import TodoList from './components/todo/TodoList';
+
+function App() {
+  return (
+      <div className='todo-app'>
+        <TodoList />
+      </div>
+  );
+}
+
+export default App;
